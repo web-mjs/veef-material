0e39be5fc8e0b86c1ea86a8dee2955fcfd976c3f
Filip Dimitrovski <filipdimitrovski22@gmail.com>
2021-07-01 14:05:31 +0200

    055a7bb8-98ea-43b5-bb21-23f59beff208

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,11 @@
 import resolve from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
 import strip from '@rollup/plugin-strip';
 import copy from 'rollup-plugin-copy';
 import {minify} from 'html-minifier-terser';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 
-
-const htmlReplacements = [
-  {
-    before: /\.\.\/node_modules/g,
-    after: 'polyfills',
-  },
-];
-
-const htmlCopyTransform = (contentsRaw) => {
-  let contents = contentsRaw.toString();
-  htmlReplacements.forEach((replacement) => {
-    contents = contents.replace(
-      replacement.before,
-      replacement.after
-    );
-  });
-  const out = minify(contents, {
-    removeComments: true,
-    collapseWhitespace: true,
-    caseSenseitive: false,
-    minifyCSS: true,
-    minifyJS: true
-  });
-
-  return out;
-};
 
 export default [
   {
@@ -45,7 +18,6 @@ export default [
     },
     plugins: [
       resolve(),
-      minifyHTML(),
       terser({ compress: { passes: 10 }, ecma: 2015, format: {ecma: 2015, comments: false, indent_level: 0} }),
       strip({
         functions: ['console.log'],
---
+++ textfield2/textfield.js
@@ -0,0 +1,36 @@
+/**
+@license
+Copyright 2019 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import { render, html, webComponents, useRef, useEffect, useState } from '@web-mjs/preact';
+import { style as myCss } from './textfield-css.js';
+
+const El = () => html`<h1>yoo</h1>`;
+const Field = (props) => {
+	const styleRef = useRef();
+	useEffect(() => {
+		styleRef.current.innerText = myCss;
+	});
+	return html`
+	<label class="mdc-text-field mdc-text-field--outlined">
+      <mwc-notched-outline class="mdc-notched-outline">
+      <span id="label" class="mdc-floating-label"><!---->Field<!----></span>
+      </mwc-notched-outline>
+      <input aria-labelledby="label" class="mdc-text-field__input" type="text" placeholder="">
+      </label>
+	<style ref=${styleRef}>input{ background: red; }</style>`;
+};
+
+webComponents.register(Field, 'mwc-textfield2', [], {shadow: true});
