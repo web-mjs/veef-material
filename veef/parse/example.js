24e921fccd6a429390a409dbcfface6cac9e36be
Filip Dimitrovski <filipdimitrovski22@gmail.com>
2021-07-12 23:36:13 +0200

    Whitespace Aggregation Saving Trailing Bumped

--- veef/parse/example.js
+++ veef/parse/example.js
@@ -1,101 +1,175 @@
-const render = (vdom) => {
-    if(vdom.charAt) return document.createTextNode(vdom);
-    let newEl = document.createElement(vdom.name || 'div');
-    vdom.props.map(x => newEl[x.name] = x.value)
-    //vdom.props.map(x => newEl.setAttribute(x.name, x.value))
-    newEl.append(...vdom.children.map(x => render(x)))
-    return newEl
-};
-const _diff = (vdom1, vdom2, w, i, buf) => {
-    if(typeof vdom1 == 'undefined') {
-        if(vdom2.name) {
-            let L = null;
-            while(L = buf.shift()){
-                if(L.name == vdom2.name) {
-                    w.appendChild(L.dom)
-                    return
+const newElement = (name, component) => {
+    let opts = null;
+    customElements.define(name, class extends HTMLElement {
+        constructor() {
+            super()
+            this._root = this.attachShadow(opts || {mode: 'open', delegatesFocus: true})
+        }
+        connectedCallback() {
+            let rerender = () => {
+                state._i = 0;
+                let newVdom = component(state)
+                diff(lastVdom, newVdom, this._root)
+                lastVdom = newVdom;
+            }
+            let state = {
+                _i: 0,
+                _data: {},
+                useState: (defaultVal) => {
+                    let key = ++state._i
+                    const set = (x) => { state._data[key] = x; rerender() }
+                    if(key in state._data) return [state._data[key], set]
+                    state._data[key] = defaultVal
+                    return [defaultVal, set]
                 }
             }
+            let lastVdom = component(state)
+            this._root.append(render(lastVdom))
         }
-        w.appendChild(render(vdom2))
+        disconnectedCallback() {
+        }
+        attributeChangedCallback(name, from, to) {
+        }
+    })
+}
+let eventHandlers = []
+const setEvent = (x, element) => {
+    if(typeof x.value == 'function' && isEvent(x)) {
+        let evName = x.name.toLowerCase().substr(2)
+        eventHandlers.filter(h => h.name == evName && h.el == element).map(x => 
+            element.removeEventListener(evName, x.fn)
+        );
+        eventHandlers.push({name: evName, el: element, fn: x.value})
+        element.addEventListener(evName, x.value)
+        return true
+    }
+    return false
+}
+const render = (vdom) => {
+    if(vdom.charAt) return document.createTextNode(vdom);
+    let name = vdom.name
+    if(name == 'root') name = 'div'
+    let newEl = document.createElement(name);
+    vdom.props.map(x => { 
+        if(!setEvent(x, newEl))
+        newEl[x.name] = x.value
+    })
+    newEl.append(...vdom.children.map(x => render(x)));
+    return newEl
+};
+const isEvent = (x) => x.name.substr(0, 2) == 'on'
+const _diff = (vdom1, vdom2, domNode, parentNode) => {
+    if(isUndef(vdom2)) {
+        if(domNode) domNode.remove()
         return
     }
-    if(vdom2.charAt && vdom1.charAt) {
+    if(isUndef(vdom1)) {
+        if(vdom2) parentNode.append(render(vdom2))
+        return
+    }
+    if(!vdom2.el || !vdom1.el) {
         if(vdom1 !== vdom2) {
-            w.insertBefore(document.createTextNode(vdom2), w.childNodes[i])
+            domNode.replaceWith(render(vdom2))
             return
         }
         return
     }
     if(vdom1.name != vdom2.name) {
-        buf.push({name: vdom1.name, dom: w.childNodes[i]})
-        w.replaceChild(render(vdom2), w.childNodes[i])
+        domNode.replaceWith(render(vdom2))
         return
     }
-    vdom2.props.filter(x => x.value != vdom2.props[x.key]).map(x => w[x.key] = x.value)
-    buf = []
-    vdom2.children.map((x, i) => _diff(vdom1.children[i], x, w, i, buf))
+    vdom2.props.filter(x => !isEvent(x) && x.value != vdom2.props[x.key]).map(x => domNode[x.key] = x.value)
+    vdom2.props.map(x => setEvent(x, domNode))
+    vdom2.children.map((x, i) => _diff(vdom1.children[i], x, domNode.childNodes[i], domNode))
+    vdom1.children.map((x, i) => _diff(x, vdom2.children[i], domNode.childNodes[i], domNode))
 }
-const diff = (vdom1, vdom2, w) => _diff(vdom1, vdom2, w, 0, [])
+const diff = (vdom1, vdom2, w) => _diff(vdom1, vdom2, w.children[0])
+
+const el = (ch) => ( {name: ch, props: [], children: [''], x:0, el:1 });
 
 const h = (strings, ...argums) => {
+    const TAG_NAME = 0, PROPS_START = 1, TAG_TEXT = 2;
     const PROP_NAME=6, PROP_EQUALS=7, PROP_Q1=8, PROP_Q2=9, EARLY=10;
+    const FUNC = 99;
+
     const NOTAGS = ['br', 'hr', 'img', 'input'];
 	const strs = [...strings];
-	const el = (ch) => ( {name: ch, props: [], children: [''], x:0 });
-	let tag = el('');
+	let tag = el('root');
 	let root = tag;
 	let tagStack = [tag];
 	let chr = null;
 
+    const children = 'children'
 	let stage = 2;
-	let _put = (x) => tag.children.push(x)
+	let _put = (x) => tag[children].push(x)
 	let put = (x) => { tag.x=_put(x); _put('') }
 	let propPut = (x) => tag.props[0].value += chr;
+    let smartTrim = (x) => {
+        if(x === '') return ''
+        let ll = x.trim()
+        let l = '', r = '';
+        if(x[0] !== ll[0]) l = ' ';
+        if(x.substr(-1) !== ll.substr(-1)) r = ' ';
+        if(ll == '' && l==r) return l
+        return l + ll + r;
+    };
+
+    const trimChildren = (node) => node.map(x => x.el ? x : smartTrim(x)).filter(x => x!=='');
 	let transitions = {
-        0: {'>': 2, ' ': 1, '/':4, 'fn': () => {tag.name += chr} },
-		1: {'>': 2, ' ': 1, '/': 4, 'fn': () => {tag.props.unshift({name: chr, value: ''}); stage = PROP_NAME}},
-		[PROP_NAME]: {'=': PROP_EQUALS, 'fn': () => {tag.props[0].name+= chr}},
-		[PROP_EQUALS]: {'"': PROP_Q1, "'": PROP_Q2, 'fn': propPut},
-		[PROP_Q1]: {'"': 1, 'fn': propPut},
-		[PROP_Q2]: {"'": 1, 'fn': propPut},
-		2: {'<': 3, 'fn': () => {
-			tag.children[tag.x] += chr
+        [TAG_NAME]: {'>': TAG_TEXT, ' ': PROPS_START, '/':4, [FUNC]: () => {tag.name += chr} },
+		[PROPS_START]: {'>': TAG_TEXT, ' ': PROPS_START, '/': 4, [FUNC]: () => {
+            tag.props.unshift({name: chr, value: ''});
+            stage = PROP_NAME}},
+		[PROP_NAME]: {'=': PROP_EQUALS, [FUNC]: () => {tag.props[0].name+= chr}},
+		[PROP_EQUALS]: {'"': PROP_Q1, "'": PROP_Q2, [FUNC]: propPut},
+		[PROP_Q1]: {'"': PROPS_START, [FUNC]: propPut},
+		[PROP_Q2]: {"'": PROPS_START, [FUNC]: propPut},
+		[TAG_TEXT]: {[FUNC]: () => {
+            if(chr == '<') {
+                tag[children] = trimChildren(tag[children])
+                stage = 3;
+                return
+            }
+			tag[children][tag.x] += chr
 			}},
-		3: {'/': 4, 'fn': () => {
-			const n = el(chr)
+		3: {'/': 4, [FUNC]: () => {
+			const newEl = el(chr)
 			tagStack.push(tag)
-			put(n)
-			tag = n
-			stage = 0
+			put(newEl)
+			tag = newEl
+			stage = TAG_NAME
 			}},
-		4: {'fn': () => {
+		4: {[FUNC]: () => {
 			if(chr != '>')  return; 
 			tag = tagStack.pop()
-			stage = 2;
+			stage = TAG_TEXT;
 			}}
 	};
 	let c = 0;
 	for(var i = 0;;i++) {
 		if(i >= strings[c].length) {
+            if(c >= argums.length) break;
 			const arg = argums[c];
-			if(!arg) break;
 			if(stage == PROP_NAME || stage == PROP_EQUALS) {
 				tag.props[0].value = arg;
-				stage = 1;
+				stage = PROPS_START;
 			}
 			if(stage == 2) {
-				put(arg)
+				put(arg.toString())
 			}
 			i = 0; c++;
-			if(c >= strings.length || i >= strings[c].length) break;
+            if(c >= strings.length) break;
 		}
 		chr  = strings[c][i];
 		let T = transitions[stage]
 		if(chr in T) stage = T[chr]
-		else T['fn']()
+		else T[FUNC]()
 	}
+    root[children] = trimChildren(root[children])
 	return root;
 }
+h.ref = (x) => x;
 
-export { h as parse, render, diff }
+export { newElement, h, render, diff }
+
+const isUndef = (x) => typeof x === 'undefined'
--- veef/parse/rollup.config.js
+++ veef/parse/rollup.config.js
@@ -1,18 +1,21 @@
 import resolve from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
+import path from 'path'
 
 export default [
   {
     // index es6
     input: './example.js',
     output: {
-      file: 'example-bundle.js',
-      format: 'es',
+		file: 'example-bundle.js',
+		format: 'es',
+		sourcemap: true,
+		sourcemapFile: path.resolve('./example-bundle.js.map')	
       //format: 'iife',
     },
     plugins: [
       resolve(),
-      terser({ compress: { passes: 10 }, ecma: 2015, format: {ecma: 2015, comments: false, indent_level: 0} }),
+      terser({ compress: { passes: 10, drop_debugger: false }, ecma: 2015, format: {ecma: 2015, comments: false, indent_level: 0} }),
     ]
   }
 ];
